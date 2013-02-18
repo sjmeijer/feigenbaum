b117e6a0f9e13a989d5c2396fa736bafa60ac137
Sam <samuelmeijer@gmail.com>
2013-02-18 13:48:04 -0800

    Last version limited precision

--- f.c
+++ f.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <string.h>
 
-#define N 15
-#define MAX_ITER 50
+#define N 10
+#define MAX_ITER 100
 
 float b(int k,float a)
 {	
@@ -11,7 +11,8 @@ float b(int k,float a)
 	if (k == 0)
 		return(0.0);
 	temp = b(k-1,a);
-	return(a - temp*temp);
+
+	return( a - (temp*temp) );
 }
 
 float bp(int k,float a)
@@ -24,46 +25,58 @@ float bp(int k,float a)
 
 int main()
 {
-	int i, j;
+	int i, j, p;
 	int  M = MAX_ITER;
 
-	float aa[N][MAX_ITER] = {0};
+	float aa[N][MAX_ITER+1] = {0};
 	float delta[N];
 	
-//	aa[0][0] = 0.0;
-//	aa[0][M] = 0.0;
-	aa[1][0] = 1.0;
+	for(j=0;j<=M;j++)
+	{
+		aa[0][j] = 0.0;
+		aa[1][j] = 1.0;
+	}
 	aa[1][M] = 1.0;
-//	for(j=0;j<=M;
-
+	aa[1][0] = 1.0;
+	aa[0][M] = 0.0;
 	delta[1] = 3.2;
-	
+
+	aa[2][0] = aa[1][M] + (aa[1][M] - aa[0][M])/(delta[1]);
+
+	printf("*****\n*****\n*****\n");
+	printf("aa[0][M] = %Lf \n",aa[0][M]);
+	printf("aa[1][M] = %Lf \n",aa[1][M]);
+	printf("aa[2][0] = %f \n\n",aa[2][0]);
+	printf("delta(1)=%Lf\n",delta[1]);
 	for(i=2;i<N;i++)
+	{
+		for(j=0;j<=M;j++)
 		{
-			aa[i][0] = aa[i-1][M] + (aa[i-1][M] - aa[i-2][M])/(delta[i-1]);
-			delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
-			printf("aa[%d][0] = %f \n",i,aa[i][0]);
-		}
-		
-	for(j=1;j<=MAX_ITER;j++)
-		{
-			printf("Interation %d\n",j);
-			for(i=2;i<N;i++)
-				{
-					aa[i][j+1] = aa[i][j] - b(1<<i,aa[i][j])/bp(1<<i,aa[i][j]);
-					printf("aa[%d][%d+1]=%f \n",i,j,aa[i][j]);
-				}
-//			memmove(a0,a,sizeof(float) * N);
-		}
-			
-		
-		
-	for(i=2;i<N;i++)
+			if(i<6)
 			{
-			delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
+				aa[i][j+1] = aa[i][j] - ( b(pow(2,i+0),aa[i][j]) / bp(pow(2,i+1),aa[i][j]) );
+			}
+			else
+			{
+				aa[i][j+1] = aa[i][j] - ( b(pow(2,i+1),aa[i][j]) / bp(pow(2,i+1),aa[i][j]) );	
+			}
+			printf("aa[%d][%d+1]=%Lf \n ",i,j,aa[i][j]);
 		}
-	
-	for(i=0;i<N;i++)
-		printf("aa[%d][END]=%f, delta[%d]=%f\n",i,aa[i][M],i,delta[i]);
+
+		printf("*****\n aa[i-1]=%Lf, aa[i-2]=%Lf, aa[i]=%Lf \n\n",aa[i-1][M],aa[i-2][M],aa[i-0][M]);
+		printf("      delta = (%Lf - %Lf)/(%Lf - %Lf)\n",aa[i-1][M],aa[i-2][M],aa[i][M],aa[i-1][M]);
+		printf("      delta = (%Lf) / (%Lf)\n\n",aa[i-1][M]-aa[i-2][M],aa[i][M]-aa[i-1][M]);
+		for(p=0;p<=i;p++)
+			printf("aa[%d][M]=%Lf, aa[%d][M-1]=%f \n",p,aa[p][M],p,aa[p][M-1]);
+
+		delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
+
+		aa[i+1][0] = aa[i][M] + (aa[i][M] - aa[i-1][M])/(delta[i]);
+		printf("***** delta[%d] = %f \n",i,delta[i]);
+		printf("aa[%d][0] = %Lf \n",i+1,aa[i+1][0]);	
+	}		
+		
+	for(i=1;i<N;i++)
+		printf("aa[%d][M]=%Lf, delta[%d]=%Lf\n",i,aa[i][M],i,delta[i]);
 }
 	
