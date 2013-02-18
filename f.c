48091d7d225c3741a0b2d5f77d49535f1c6293cf
Sam <samuelmeijer@gmail.com>
2013-02-18 15:05:17 -0800

    Rewrote b function to decrease recursion and increase time to stack overflow

--- f.c
+++ f.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#include <gmp.h>
+#include <time.h>
 
-#define N 10
-#define MAX_ITER 500
+#define N 19
+#define MAX_ITER 5
 
-mpf_t b(int k, mpg double a)
-{	
-	long double temp;
-	if (k == 0)
-		return(0.0);
-	temp = b(k-1,a);
-
-	return( a - (temp*temp) );
-}
-
-float bp(int k, long double a)
-{
-	if (k == 0)
-		return(0.0);
-
-	return(1.0 - 2.0 * bp(k-1,a) * b(k-1,a));
-}
+long double b(unsigned long long int k, const long double &a);
+long double bp(unsigned long long int k, const long double &a);
+void saver(long double delta,int i, time_t time);
 
 int main()
 {
-	int i, j, p;
+	int i, j;
 	int  M = MAX_ITER;
 
-	long double aa[N][MAX_ITER+1] = {0};
+//	time_t start, finish, runtime;
+	time_t runtime;
+
+	long double aa[N][MAX_ITER+1] = {{0}};
 	long double delta[N];
-	
-	long double test;
 
 	printf("\e[4;91mBeginning of Run\e[0m\n");
 
@@ -42,44 +29,80 @@ int main()
 		aa[1][j] = 1.0;
 	}
 	
-//	aa[1][M] = 1.0;
-//	aa[1][0] = 1.0;
-//	aa[0][M] = 0.0;
 	delta[1] = 3.20;
 
 	aa[2][0] = aa[1][M] + (aa[1][M] - aa[0][M])/(delta[1]);
-	aa[2][0] = 1.0 + (1.0-0.0)/3.2;
-//	printf("Test = %3.20Lf \n",test);
-//	aa[2][0]=test;
 
 	printf("\n\n");
-	printf("aa[0][M] = %3.20Lf \n",aa[0][M]);
-	printf("aa[1][M] = %3.20Lf \n",aa[1][M]);
-	printf("aa[2][0] = %3.20Lf \n\n",aa[2][0]);
-	printf("delta(1)=%3.20Lf\n",delta[1]);
-	for(i=2;i<N;i++)
+	printf("aa[0][M] = %3.30Lf \n",aa[0][M]);
+	printf("aa[1][M] = %3.30Lf \n",aa[1][M]);
+	printf("aa[2][0] = %3.30Lf \n\n",aa[2][0]);
+	printf("delta(1)=%3.30Lf\n",delta[1]);
+
+	for(i=2;i<N-1;i++)
 	{
-		printf("\e[1;34mIteration (i) %d\e[0m\n",i);
+		runtime = time(NULL);
+		printf("\e[1;34mIteration (%d)  \e[0m\n",i);
 	
-		for(j=0;j<=M;j++)
-		{
-			aa[i][j+1] = aa[i][j] - ( b(pow(2,i),aa[i][j]) / bp(pow(2,i),aa[i][j]) );
-	//		printf("aa[%d][%d+1]=%3.10f \n ",i,j,aa[i][j]);
-		}
-		printf("*****\n aa[i-2]=%Lf, aa[i-2]=%Lf, aa[i]=%Lf \n\n",aa[i-2][M],aa[i-2][M],aa[i-0][M]);
-	//	printf("      delta = (\n%3.15Lf - \n%3.15Lf)\n/(\n%3.15Lf - \n%3.15Lf)\n",aa[i-1][M],aa[i-2][M],aa[i][M],aa[i-1][M]);
-		printf("      delta = \n(%3.20Lf)\n / \n(%3.20Lf)\n\n",aa[i-1][M]-aa[i-2][M],aa[i][M]-aa[i-1][M]);
-	//	for(p=0;p<=i;p++)
-	//		printf("aa[%d][M]=%Lf, aa[%d][M-1]=%Lf \n",p,aa[p][M],p,aa[p][M-1]);
+		for(j=0;j<M;j++)
+			aa[i][j+1] = aa[i][j] - ( b(1<<i,aa[i][j]) / bp(1<<i,aa[i][j]) );		
 
 		delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
 
 		aa[i+1][0] = aa[i][M] + (aa[i][M] - aa[i-1][M])/(delta[i]);
-		printf("***** delta[%d] = %3.15Lf \n",i,delta[i]);
-		printf("aa[%d][0] = %Lf \n",i+1,aa[i+1][0]);	
+//		finish = time(NULL);
+		runtime = time(NULL) - runtime;
+
+		printf("*** delta[%d-1] = %3.30Lf \n",i,delta[i-1]);
+		printf("***   delta[%d] = %3.30Lf \n",i,delta[i]);
+		printf("\n*** aa[%d-1][0] = %3.30Lf \n",i+1,aa[i][0]);	
+		printf("***   aa[%d][0] = %3.30Lf \n",i+1,aa[i+1][0]);	
+		printf("***   Time to finish %dth loop: %lu seconds\n\n",i,(unsigned long)runtime);
+
+		saver(delta[i],i,runtime);
 	}		
 		
-	for(i=1;i<N;i++)
-		printf("aa[%d][M]=%3.20Lf, delta[%d]=%3.20Lf\n",i,aa[i][M],i,delta[i]);
+	for(i=1;i<N-1;i++)
+		printf("aa[%d][M]=%3.30Lf, delta[%d]=%3.30Lf\n",i,aa[i][M],i,delta[i]);
+	printf("\n");
+
+//	printf("Segmentation fault.\n");
+	return 0;
 }
 
+long double b(unsigned long long int k, const long double &a)
+{	
+        long double val = 0;
+        while(k > 0) {
+            val = a - val*val;
+            k--;
+        }
+        return val;
+//	if (k == 0)
+//		return(0.0);
+//	return( a - pow(b(k-1,a),2) );
+}
+
+long double bp(unsigned long long int k, const long double &a)
+{
+	if (k == 0)
+		return(0.0);
+	return(1.0 - 2.0 * bp(k-1,a) * b(k-1,a));
+//	long double temp = 0;
+//	while(k>0)
+//	{
+//		temp = 1.0 - 2.0 * temp*b(k-1,a);
+//		k--;
+//	}
+//	return temp;
+}
+
+void saver(long double delta, int i, time_t time)
+{
+	FILE *file;
+	file = fopen("output.txt","a");
+	fprintf(file,"\nAfter %d iterations (in %llu seconds):\n%3.30Lf\n",i,(unsigned long long int)time,delta);
+	fclose(file);
+	
+	return;
+}
