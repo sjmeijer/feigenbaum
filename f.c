412ed6ac2e7f81d2791e82f9f7f6f88350803c55
Sam <samuelmeijer@gmail.com>
2013-02-18 13:42:34 -0800

    Third version

--- f.c
+++ f.c
@@ -2,57 +2,47 @@
 #include <math.h>
 #include <string.h>
 
-#define N 10
-#define MAX_ITER 5
+#define N 15
+#define MAX_ITER 50
 
 float b(int k,float a)
-<<<<<<< HEAD
 {	
 	float temp;
 	if (k == 0)
 		return(0.0);
 	temp = b(k-1,a);
 	return(a - temp*temp);
-=======
-{
-        float temp;
-        if (k == 0)
-                return(0.0);
-        temp = b(k-1,a);
-        return(a - temp*temp);
->>>>>>> a94d233e62a2649ce02cade3d3867ad5a0bf42a7
 }
 
 float bp(int k,float a)
 {
-<<<<<<< HEAD
 	if (k == 1)
 		return(0.0);
 
 	return(1.0 - 2.0 * bp(k-1,a) * b(k-1,a));
-=======
-        if (k == 1)
-                return(0.0);
-
-        return(1.0 - 2.0 * bp(k-1,a) * b(k-1,a));
->>>>>>> a94d233e62a2649ce02cade3d3867ad5a0bf42a7
 }
 
 int main()
 {
-<<<<<<< HEAD
 	int i, j;
-	float a[N], anext[N];
+	int  M = MAX_ITER;
+
+	float aa[N][MAX_ITER] = {0};
 	float delta[N];
 	
-	a[0] = anext[0] = 0.0;
-	a[1] = anext[1] = 1.0;
+//	aa[0][0] = 0.0;
+//	aa[0][M] = 0.0;
+	aa[1][0] = 1.0;
+	aa[1][M] = 1.0;
+//	for(j=0;j<=M;
+
 	delta[1] = 3.2;
 	
 	for(i=2;i<N;i++)
 		{
-			a[i] = a[i-1] + (a[i-1] - a[i-2])/(delta[i-1]);
-			delta[i] = (a[i-1] - a[i-2])/(a[i]-a[i-1]);
+			aa[i][0] = aa[i-1][M] + (aa[i-1][M] - aa[i-2][M])/(delta[i-1]);
+			delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
+			printf("aa[%d][0] = %f \n",i,aa[i][0]);
 		}
 		
 	for(j=1;j<=MAX_ITER;j++)
@@ -60,55 +50,20 @@ int main()
 			printf("Interation %d\n",j);
 			for(i=2;i<N;i++)
 				{
-					anext[i] = a[i] - b(1<<i,a[i])/bp(1<<i,a[i]);
+					aa[i][j+1] = aa[i][j] - b(1<<i,aa[i][j])/bp(1<<i,aa[i][j]);
+					printf("aa[%d][%d+1]=%f \n",i,j,aa[i][j]);
 				}
-			memmove(a,anext,sizeof(float) * N);
+//			memmove(a0,a,sizeof(float) * N);
 		}
 			
 		
 		
 	for(i=2;i<N;i++)
-		{
-			delta[i] = (a[i-1] - a[i-2])/(a[i]-a[i-1]);
+			{
+			delta[i] = (aa[i-1][M] - aa[i-2][M])/(aa[i][M]-aa[i-1][M]);
 		}
 	
 	for(i=0;i<N;i++)
-		printf("a[%d]=%f, delta[%d]=%f\n",i,a[i],i,delta[i]);
+		printf("aa[%d][END]=%f, delta[%d]=%f\n",i,aa[i][M],i,delta[i]);
 }
 	
-=======
-        int i, j;
-        float a[N], anext[N];
-        float delta[N];
-
-        a[0] = anext[0] = 0.0;
-        a[1] = anext[1] = 1.0;
-        delta[1] = 3.2;
-
-        for(i=2;i<N;i++)
-                {
-                        a[i] = a[i-1] + (a[i-1] - a[i-2])/(delta[i-1]);
-                        delta[i] = (a[i-1] - a[i-2])/(a[i]-a[i-1]);
-                }
-
-        for(j=1;j<=MAX_ITER;j++)
-                {
-                        printf("Interation %d\n",j);
-                        for(i=2;i<N;i++)
-                                {
-                                        anext[i] = a[i] - b(1<<i,a[i])/bp(1<<i,a[i]);
-                                }
-                        memmove(a,anext,sizeof(float) * N);
-                }                                                               
-
-
-
-        for(i=2;i<N;i++)
-                {
-                        delta[i] = (a[i-1] - a[i-2])/(a[i]-a[i-1]);
-                }
-
-        for(i=0;i<N;i++)
-                printf("a[%d]=%f, delta[%d]=%f\n",i,a[i],i,delta[i]);
-}
->>>>>>> a94d233e62a2649ce02cade3d3867ad5a0bf42a7
